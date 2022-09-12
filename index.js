b727fc5c131dc7a6f66b081ebd598bcdebacab64
sagar-uprety <sagarupreti100@gmail.com>
2022-09-12 13:16:55 +0545

    demo app configured

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const Todo = require("./models/todo");
+const PORT = process.env.PORT || 4000;
+// const DB_String = process.env.CUSTOMCONNSTR_mongodb_url;
+
+// Example MongoDB Database Connection Address
+const DB_String =
+  "mongodb://username:password@host_address:PORT/DB_NAME?OPTIONS";
+
+app.use("/", express.static(path.resolve(__dirname, "assets")));
+app.use(bodyParser.json());
+
+//Azure Cosmos DB Config
+mongoose.connect(DB_String, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//Read
+app.get("/api/get", async (req, res) => {
+  const records = await Todo.find({});
+  console.log(process.env.CUSTOMCONNSTR_mongodb_url);
+  res.json(records);
+});
+
+//Create Todos
+app.post("/api/create", async (req, res) => {
+  const record = req.body;
+  console.log(record);
+
+  const response = await Todo.create(record);
+  console.log(response);
+
+  res.json({ status: "Todo Created!" });
+});
+
+//Delete Todos
+app.post("/api/delete", async (req, res) => {
+  const { record } = req.body;
+  console.log(record, "/api/delete");
+
+  const response = await Todo.deleteOne({ record });
+
+  console.log(response, "/api/delete repsonse");
+
+  res.json({ status: "ok" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
---
+++ models/todo.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const TodoSchema = new mongoose.Schema(
+  {
+    record: { type: String, required: true },
+    date: {
+      type: Number,
+      default: Date.now,
+    },
+  },
+  { collection: "todos" }
+);
+
+const model = mongoose.model("TodoModel", TodoSchema);
+
+module.exports = model;
